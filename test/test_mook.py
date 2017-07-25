f5c23336aad69f6e503c44f56791a2cce0348dfa
James Graham <J.A.Graham@live.co.uk>
2017-07-25 11:23:43 +0100

    Add initial reading of attacks from file

--- test/test_mook.py
+++ test/test_mook.py
@@ -1,5 +1,5 @@
-import unittest
 import os
+import unittest
 
 from ttbmook.mook import Mook
 
@@ -8,7 +8,21 @@ class MookTest(unittest.TestCase):
     def test_json_load_mooks(self):
         mooks = Mook.mooks_from_json(os.path.join("data", "mooks.json"))
         mook = mooks[0]
+
         self.assertEqual("Hanged Man", mook.name)
+        self.assertEqual(10, mook.willpower)
+        self.assertEqual(5, mook.defence)
+        self.assertEqual(7, mook.hp)
+
+    def test_json_load_attacks(self):
+        mooks = Mook.mooks_from_json(os.path.join("data", "mooks.json"))
+        mook = mooks[0]
+
+        self.assertEqual(1, len(mook.attacks))
+        self.assertEqual("Fists", mook.attacks[0].name)
+        self.assertEqual(2, mook.attacks[0].range)
+        self.assertEqual(1, mook.attacks[0].ap)
+        self.assertEqual("melee", mook.attacks[0].skill)
 
 
 if __name__ == '__main__':
--- ttbmook/mook.py
+++ ttbmook/mook.py
@@ -2,8 +2,13 @@ import json
 
 
 class Mook:
-    def __init__(self, name):
+    def __init__(self, name, willpower, defence, hp, attacks=None):
         self.name = name
+        self.willpower = willpower
+        self.defence = defence
+        self.hp = hp
+
+        self.attacks = attacks
 
     @classmethod
     def mooks_from_json(cls, filename):
@@ -18,15 +23,32 @@ class Mook:
 
         mooks = []  # TODO should this be a dictionary of {name: Mook}
         for json_mook in json_all.mooks:
-            mooks.append(cls.mook_from_json(json_mook))
+            mooks.append(cls.from_json(json_mook))
 
         return mooks
 
     @classmethod
-    def mook_from_json(cls, json_block):
-        mook = cls(json_block.name)
+    def from_json(cls, json_block):
+        attacks = []
+        for json_attack in json_block.attacks:
+            attacks.append(Attack.from_json(json_attack))
+
+        mook = cls(name=json_block.name, willpower=json_block.willpower,
+                   defence=json_block.defence, hp=json_block.HP, attacks=attacks)
         return mook
 
+
+class Attack:
+    def __init__(self, name, range, ap, skill):
+        self.name = name
+        self.range = range
+        self.ap = ap
+        self.skill = skill
+
+    @classmethod
+    def from_json(cls, json_block):
+        return cls(name=json_block.name, range=json_block.range,
+                   ap=json_block.AP, skill=json_block.skill)
 
 class SkillCheck:
     """A general skill check."""
@@ -36,7 +58,7 @@ class SkillCheck:
         self.target_suit = target_suit
 
 
-class Attack(SkillCheck):
+class AttackCheck(SkillCheck):
     """An attack skill check.  Provides description and handles damage."""
     def __init__(self, target_number, modifier=0, target_suit=None):
         super().__init__(target_number, modifier, target_suit)
@@ -44,6 +66,7 @@ class Attack(SkillCheck):
     @classmethod
     def from_json(cls, json_block):
         raise NotImplementedError
+
 
 
 class AttrDict(dict):
